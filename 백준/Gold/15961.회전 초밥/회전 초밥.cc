5ca9a61f8b118e57a5d67bae15b216326d600608
Jiyong Seo <54535159+sjyn01@users.noreply.github.com>
2022-01-26 11:53:49 +0900

    [Gold IV] Title: 회전 초밥, Time: 948 ms, Memory: 13752 KB -BaekjoonHub

--- 백준/Gold/15961.회전 초밥/회전 초밥.cc
+++ 백준/Gold/15961.회전 초밥/회전 초밥.cc
@@ -1,74 +1,40 @@
 #include <iostream>
-#include <queue>
-#include <vector>
-#include <stack>
-#include <string>
 #include <cstring>
-#include <sstream>
-#include <set>
-#include <unordered_set>
-#include <map> 
 #include <algorithm>
-#include <cmath>
-#include <ctime>
-#define fastio ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
-#define ENDL cout << endl
-#define ll long long
-#define ull unsigned long long
-#define INF 987654321
-#define Mod 1000000007
-#define endl '\n'
 
 using namespace std;
 
 int N, d, k, c;
-vector<int> ps;
-int visited[30001];
-
-void input() {
-	cin >> N >> d >> k >> c;
-	ps = vector<int>(N, 0);
-	for (int i = 0; i < N; i++) {
-		cin >> ps[i];
-	}
-}
-
-int solution() {
-	int cnt = 0;
-	int answer;
-	for (int left = 0; left < k; left++) {
-		if (!visited[ps[left]]) {
-			cnt++;
-		}
-		visited[ps[left]]++;
-	}
-	if (visited[c] == 0) answer = cnt + 1;
-	else answer = cnt;
-
-	int left = 0, right = k-1;
-	while (1) {
-		// 가장 왼쪽에 있던 초밥을 제거
-		visited[ps[left]]--;
-		if(visited[ps[left]]==0) cnt--;
-		// 가장 왼쪽이 마지막 위치까지 올 경우 모든 구간 확인 
-		left++;
-		if (left == N) break;
-		// 오른쪽으로 이동 -> 마지막까지 이동했으면 0부터 다시 확인
-		right++;
-		if (right == N) right = 0;
-		if (visited[ps[right]] == 0) cnt++;
-		visited[ps[right]]++;
-
-		if (visited[c] == 0) answer = max(answer, cnt + 1);
-		else answer = max(answer, cnt);
-	}
-	return answer;
-}
+int arr[3000001];
+int chk[3001];
+int ans = 0;
 
 int main() {
-	fastio;
-	input();
-	cout << solution();
+	cin >> N >> d >> k >> c;
 
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+
+
+	for (int i = 0; i < k; i++) {
+		chk[arr[i]]++;
+		if (chk[arr[i]] == 1) ans++;
+	}
+
+	int temp = ans;
+
+	for (int i = 0; i < N - 1; i++) {
+		chk[arr[i]]--;
+		if (chk[arr[i]] == 0) temp--;
+
+		chk[arr[(i + k) % N]]++;
+		if (chk[arr[(i + k) % N]] == 1) temp++;
+
+		if (chk[c]) ans = max(ans, temp);
+		else ans = max(ans, temp + 1);
+	}
+
+	cout << ans << '\n';
 	return 0;
 }
