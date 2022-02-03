1623b7b9ad238fe51f9c9691850bb8a90c04ad63
Jiyong Seo <54535159+sjyn01@users.noreply.github.com>
2022-02-03 15:19:26 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15655.N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+vector<int> v, arr;
+bool chk[9];
+
+void back_tracking(int idx, int cnt) {
+	if (cnt == M) {
+		for (int i = 0; i < v.size(); i++) cout << v[i] << ' ';
+		cout << '\n';
+		return;
+	}
+
+	for (int i = idx; i < N; i++) {
+		if (chk[i]) continue;
+		v.push_back(arr[i]);
+		chk[i] = true;
+		back_tracking(i+1, cnt + 1);
+		chk[i] = false;
+		v.pop_back();
+	}
+	return;
+}
+int main() {
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) {
+		int num;
+		cin >> num;
+		arr.push_back(num);
+	}
+	sort(arr.begin(), arr.end());
+	back_tracking(0, 0);
+}
+
