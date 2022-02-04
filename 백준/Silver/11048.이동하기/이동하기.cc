dacb63287c0f707ceaf4640522bbbef0591588e7
Jiyong Seo <54535159+sjyn01@users.noreply.github.com>
2022-02-04 11:20:18 +0900

    [Silver I] Title: 이동하기, Time: 64 ms, Memory: 5936 KB -BaekjoonHub

--- 백준/Silver/11048.이동하기/이동하기.cc
+++ 백준/Silver/11048.이동하기/이동하기.cc
@@ -2,7 +2,6 @@
 #include <algorithm>
 using namespace std;
 
-int arr[1001][1001];
 int ans[1001][1001];
 int N, M;
 int main() {
@@ -11,19 +10,11 @@ int main() {
 	cin >> N >> M;
 	for (int i = 1; i <= N; i++) {
 		for (int j = 1; j <= M; j++) {
-			cin >> arr[i][j];
+			int num;
+			cin >> num;
+			ans[i][j] = max(ans[i - 1][j - 1], max(ans[i - 1][j], ans[i][j - 1])) + num;
 		}
 	}
-
-	ans[1][1] = arr[1][1];
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= M; j++) {
-			if (i == 1 && j == 1) continue;
-
-			ans[i][j] = max(ans[i-1][j-1],max(ans[i-1][j],ans[i][j-1])) + arr[i][j];
-		}
-	}
-
 	cout << ans[N][M] << '\n';
 	return 0;
 }
